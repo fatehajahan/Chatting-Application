953dce2e8ee518f86db6d8cc6882fdcf9d50f571
Fatehajahan2002 <fatehajahan2002@gmail.com>
2024-12-18 20:41:45 +0600

    done

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { createRef, useState } from 'react'
 import sideprofile from '../../assets/sideProfile.jpeg'
+import defaultimage from '../../assets/default.png'
 import { IoHomeOutline } from "react-icons/io5";
 import { AiOutlineMessage } from "react-icons/ai";
 import { IoIosSettings } from "react-icons/io";
@@ -7,7 +8,8 @@ import { CiBellOn } from "react-icons/ci";
 import { ImExit } from "react-icons/im";
 import { Link } from 'react-router-dom';
 import { FaCloudUploadAlt } from "react-icons/fa";
-
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
 
 const SideBar = () => {
     const [upload, setUpload] = useState(false)
@@ -55,6 +57,33 @@ const SideBar = () => {
         setmessage(false)
         setbell(false)
         setsetting(false)
+    }
+
+    const [image, setImage] = useState();
+    const [cropData, setCropData] = useState("");
+    const cropperRef = createRef();
+
+    const onChange = (e) => {
+        e.preventDefault();
+        let files;
+        console.log(e, 'eee');
+
+        if (e.dataTransfer) {
+            files = e.dataTransfer.files;
+        } else if (e.target) {
+            files = e.target.files;
+        }
+        console.log(files, 'adds');
+        const reader = new FileReader();
+        reader.onload = () => {
+            setImage(reader.result);
+            console.log(reader.result);
+        };
+        reader.readAsDataURL(files[0]);
+    };
+
+    const handleRemove = () =>{
+        setImage(false)
     }
     return (
         <div>
@@ -140,25 +169,57 @@ const SideBar = () => {
                     <div className='bg-white w-[500px] py-[20px] px-[20px] rounded-md text-center'>
                         <h1 className='text-[30px] font-nuni font-semibold text-black'>Forgot Password</h1>
 
-                        <div className="email md:w-[370px] w-[295px] mx-auto relative pt-[70px]">
+                        <div className="choosePht md:w-[370px] w-[295px] mx-auto relative pt-[0px]">
+
+                            {
+                                image
+                                    ? <div className='overflow-hidden '>
+                                        <div
+                                            className="img-preview w-[100px] h-[100px] mx-auto overflow-hidden " />
+                                    </div>
+                                    : <div className='overflow-hidden '>
+                                        <img src={defaultimage} alt="" className='mx-auto' />
+                                    </div>
+                            }
                             <label htmlFor="" className='font-open font-normal text-[20px] text-black '>
                                 Choose a Photo
                             </label>
+
+                            {
+                                image &&
+                                <Cropper
+                                    ref={cropperRef}
+                                    style={{ height: 400, width: "100%" }}
+                                    zoomTo={0.5}
+                                    initialAspectRatio={1}
+                                    preview=".img-preview"
+                                    src={image}
+                                    viewMode={1}
+                                    minCropBoxHeight={10}
+                                    minCropBoxWidth={10}
+                                    background={false}
+                                    responsive={true}
+                                    autoCropArea={1}
+                                    checkOrientation={false} // https://github.com/fengyuanchen/cropperjs/issues/671
+                                    guides={true}
+                                />
+                            }
+
                             <input
-                                // onChange={Email}
+                                onChange={onChange}
                                 type="file"
                                 className='py-[16px] w-full border-maroon border-opacity-[0.5] border-b-[3px] block focus:border-[#03014C] focus:outline-none placeholder:text-maroon placeholder:font-open placeholder:font-semibold ' />
                         </div>
 
                         <div className="flex gap-x-[20px]">
                             <p
-                                // onClick={setPassword}
                                 className=' cursor-pointer py-[15px] w-full text-center mt-[55px] bg-maroon font-open text-[20px] font-semibold text-white rounded-[8px] transition duration-300 hover:bg-maroon hover:bg-opacity-[0.5] hover:text-maroon '>Upload
                             </p>
 
-                            <Link
+                            <p
+                                onClick={handleRemove}
                                 className=' cursor-pointer py-[15px] w-full text-center mt-[55px] bg-maroon font-open text-[20px] font-semibold text-white rounded-[8px] transition duration-300 hover:bg-maroon hover:bg-opacity-[0.5] hover:text-maroon '> Remove Photo
-                            </Link>
+                            </p>
                         </div>
                     </div>
                 </div>
